2b31c9c22655cca24cf41bfcf28d868740a32038
Geoffray-buhler <geoffray.buhler@gmail.com>
2020-08-11 06:31:43 +0200

    first commit

---
+++ precache-manifest.fa64de2daac9cbb3c70638e3e186414d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28244178ace8624cccd8aaf27d7dd6b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3856c46ec6c8f9d8bc57",
+    "url": "/static/css/main.ec94164b.chunk.css"
+  },
+  {
+    "revision": "ca52cc2323cebe93661e",
+    "url": "/static/js/2.f9b98089.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f9b98089.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3856c46ec6c8f9d8bc57",
+    "url": "/static/js/main.6ab69356.chunk.js"
+  },
+  {
+    "revision": "03031250b05634549845",
+    "url": "/static/js/runtime-main.9227c2ae.js"
+  }
+]);
